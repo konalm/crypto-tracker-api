516636f11de44625eb2a786b7c87b07ab558dfa4
Lloyd Moore <manchesterboy@gmail.com>
2019-02-17 21:26:08 +0000

    abstracts rate duplication

--- fetchCryptoRates/fetchBitcoinRate.go
+++ fetchCryptoRates/fetchBitcoinRate.go
@@ -6,32 +6,12 @@ import (
 	. "fmt"
 	. "io/ioutil"
 	"net/http"
+	"stelita-api/fetchCryptoRates/fetchrate"
 )
 
 func FetchBitcoinRate() structs.BitcoinRate {
-	client := &http.Client{}
-	request, err := http.NewRequest(
+  return fetchrate(
 		"GET",
-		"https://rest.coinapi.io/v1/exchangerate/BTC/USD", nil,
+		"https://rest.coinapi.io/v1/exchangerate/BTC/USD",
 	)
-	request.Header.Set("X-CoinApi-Key", `E4C3D4AE-29D8-4A9F-BD36-EB367D836532`)
-
-	resp, err := client.Do(request)
-	if err != nil {
-		Println("handle http error")
-	}
-
-	if resp.StatusCode != 200 {
-		panic("ERROR fetching bitcoin rate from coinapi")
-	}
-
-	body, err := ReadAll(resp.Body)
-	if err != nil {
-		Println("ERROR reading response body")
-	}
-	jsonBody := string(body)
-	var bitcoinRate structs.BitcoinRate
-	Unmarshal([]byte(jsonBody), &bitcoinRate)
-
-	return bitcoinRate
 }
--- fetchCryptoRates/fetchCryptoRates.go
+++ fetchCryptoRates/fetchCryptoRates.go
@@ -6,6 +6,7 @@ import (
   "fmt"
   "io/ioutil"
   "stelita-api/structs"
+	"stelita-api/fetchCryptoRates/fetchrate"
 )
 
 
@@ -18,28 +19,8 @@ type ApiResponse struct {
  *
  */
 func FetchCryptoRates() []structs.BitcoinRate {
-  client := &http.Client{}
-  request, err := http.NewRequest("GET", "https://rest.coinapi.io/v1/exchangerate/BTC", nil)
-  request.Header.Set("X-CoinApi-Key", `E4C3D4AE-29D8-4A9F-BD36-EB367D836532`)
-
-  resp, err := client.Do(request)
-  if err != nil {
-    fmt.Println(err.Error())
-  }
-
-  if resp.StatusCode != 200 {
-    panic("ERROR fetching crypto rates")
-  }
-
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    fmt.Println("ERROR reading response body")
-  }
-
-  jsonBody := string(body)
-  var apiResponse ApiResponse
-
-  json.Unmarshal([]byte(jsonBody), &apiResponse)
-
-  return apiResponse.Rates
+  return fetchrate(
+		"GET",
+		"https://rest.coinapi.io/v1/exchangerate/BTC",
+	)
 }
