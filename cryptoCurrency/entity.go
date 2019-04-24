8b4b8cc0adc004992298aa5ea4de6f36cc02a560
root <root@findmydi.staging>
2019-04-24 20:09:07 +0000

    get crypto currency data endpoint

--- cryptoCurrency/entity.go
+++ cryptoCurrency/entity.go
@@ -12,5 +12,6 @@ type CryptoCurrencyItemData struct {
   Volume24h float64
   ClosingPrice float64
   RSI float64
+  Img string
   Rates []structs.CryptoRate
 }
--- cryptoCurrency/repository.go
+++ cryptoCurrency/repository.go
@@ -12,8 +12,11 @@ func GetCryptoCurrencyData(symbol string) CryptoCurrencyItemData {
   conn := db.Conn()
   defer conn.Close()
 
-  query :=  `SELECT name, symbol, rank, market_cap, volume_24h
+  query :=  `SELECT name, symbol, rank, market_cap, volume_24h,
+    logo.img
     FROM ranked_crypto_currencies
+    LEFT JOIN crypto_currency_logos logo
+      on logo.currency = name
     WHERE symbol = ?`
 
   stmt := conn.QueryRow(query, symbol)
@@ -25,6 +28,7 @@ func GetCryptoCurrencyData(symbol string) CryptoCurrencyItemData {
     &cryptoCurrencyData.Rank,
     &cryptoCurrencyData.MarketCap,
     &cryptoCurrencyData.Volume24h,
+    &cryptoCurrencyData.Img,
   )
 
   if err != nil {
